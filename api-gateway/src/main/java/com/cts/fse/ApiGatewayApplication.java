9b62dc4e91ba5bc685fbc25c1183853b80a05c4f
Abhijit <abhikulkarni.1988@gmail.com>
2022-11-19 22:51:39 +0530

    added cors configuration to API gateway

--- api-gateway/src/main/java/com/cts/fse/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/cts/fse/ApiGatewayApplication.java
@@ -5,6 +5,9 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -13,6 +16,12 @@ public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public SecurityWebFilterChain corsWebfilterSpringSecurityFilterChain(ServerHttpSecurity http) {
+        http.csrf().disable();
+        return http.build();
     }
 
 }
