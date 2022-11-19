802f2c1251a62b805c349fbe7f8b3380d4d5e102
Abhijit <abhikulkarni.1988@gmail.com>
2022-11-19 19:34:04 +0530

    Static data for tourist places

---
+++ data.sql
@@ -0,0 +1,5 @@
+insert into tourist_places (id,place_name) values (1,'Andaman');
+insert into tourist_places (id,place_name) values (2,'Thailand');
+insert into tourist_places (id,place_name) values (3,'Dubai');
+insert into tourist_places (id,place_name) values (4,'Singapore');
+insert into tourist_places (id,place_name) values (5,'Malaysia');
