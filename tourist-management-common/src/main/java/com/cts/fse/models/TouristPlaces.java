19d01ceb8546255471ef86dc8351ad84118d72fd
Abhijit <abhikulkarni.1988@gmail.com>
2023-01-01 20:43:48 +0530

    Added code to insert static data while booting up service

--- tourist-management-common/src/main/java/com/cts/fse/models/TouristPlaces.java
+++ tourist-management-common/src/main/java/com/cts/fse/models/TouristPlaces.java
@@ -1,7 +1,9 @@
 package com.cts.fse.models;
 
 import com.cts.fse.domain.BaseEntity;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,8 @@ import java.io.Serializable;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TouristPlaces extends BaseEntity implements Serializable {
 
     @Id
--- tourist-management-query/src/main/java/com/cts/fse/TouristManagementQueryApplication.java
+++ tourist-management-query/src/main/java/com/cts/fse/TouristManagementQueryApplication.java
@@ -1,25 +1,33 @@
 package com.cts.fse;
 
 import com.cts.fse.api.queries.*;
+import com.cts.fse.domain.TouristPlacesRepository;
 import com.cts.fse.infrastructure.QueryDispatcher;
+import com.cts.fse.models.TouristPlaces;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableJpaAuditing
-public class TouristManagementQueryApplication {
+public class TouristManagementQueryApplication implements CommandLineRunner {
 
     @Autowired
     private QueryDispatcher queryDispatcher;
 
     @Autowired
     private QueryHandler queryHandler;
+
+    @Autowired
+    private TouristPlacesRepository touristPlacesRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(TouristManagementQueryApplication.class, args);
@@ -34,4 +42,16 @@ public class TouristManagementQueryApplication {
         queryDispatcher.registerHandler(FindAllTouristPlacesQuery.class, queryHandler::handle);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        long count=this.touristPlacesRepository.count();
+        if(count==0){
+            List<TouristPlaces> placesList= Arrays.asList(new TouristPlaces(1L,"Andaman"),
+                                                            new TouristPlaces(2L,"Thailand"),
+                                                                new TouristPlaces(3L,"Dubai"),
+                                                                    new TouristPlaces(4L,"Singapore"),
+                                                                        new TouristPlaces(5L,"Malaysia"));
+            this.touristPlacesRepository.saveAll(placesList);
+        }
+    }
 }
